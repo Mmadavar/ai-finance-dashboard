92f2c2bbd92832550e023e683b45cd8e1c170710
Moses Madavaram <mosesgab@gmail.com>
2025-04-16 15:22:10 -0400

    Backend and Pinecone Setup
    Setting up Pinecone and the FastApi for the backend.

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,7 @@
 'use client'
 
 
-
+import data_from_web from "Python_Algorithms/Pinecone_Setup.py"
 import Image from "next/image";
 import {stockData} from './stock.js';
 import {useState} from 'react';
@@ -17,6 +17,7 @@ export default function Home() {
     function generateNews() {
            const news = "Market is really high today."
            setNews(news)
+           data_from_web.append("Market is really high today.", stockData)
     }
 
     async function generateStockinfo() {
@@ -45,7 +46,7 @@ export default function Home() {
 
                 <div className="content-center">
                     <h1 className="text-3xl font-bold underline">
-                    Here's some investment insights.
+                    Here's some investment insights. Click "Generate Reports" to find s
                 </h1>
             </div>
             <button onClick={generateNews} className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Generate Reports</button>
